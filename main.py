c360f249f0a370826bf505a5d68909f91d388687
JuliaFSO <juliafso@gmail.com>
2024-07-08 19:14:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from generate_csv import generate_csv
-from utils import random_word
+from utils import random_word, get_cat
 from frames import create_main_window
 
 def main():
     generate_csv()
     word = random_word()
+    category = get_cat(word)
     print(word)
+    print(category)
     root = create_main_window(word)
     root.mainloop()
 
